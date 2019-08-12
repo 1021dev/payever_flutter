1c92dfc19e52da398c0b47bc8c80eb96354366ed
softdev112 <mappdevm@gmail.com>
2019-08-12 11:11:03 +0200

    Added new avatar card template

--- lib/views/customelements/dashboard_card_templates.dart
+++ lib/views/customelements/dashboard_card_templates.dart
@@ -4,12 +4,14 @@ import 'package:payever/utils/utils.dart';
 
 class AvatarDescriptionCard extends StatelessWidget {
   final ImageProvider image;
-  final String  imageTitle;
+  final String imageTitle;
   final String _title;
   final String _detail;
-  AvatarDescriptionCard(this.image,this._title,this._detail,{this.imageTitle})
-    :assert(image != null || imageTitle != null);
-       
+
+  AvatarDescriptionCard(this.image, this._title, this._detail,
+      {this.imageTitle})
+      : assert(image != null || imageTitle != null);
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -18,16 +20,31 @@ class AvatarDescriptionCard extends StatelessWidget {
           backgroundColor: Colors.grey.withOpacity(0.5),
           radius: AppStyle.dashboardRadius(),
           backgroundImage: image,
-          child: imageTitle!=null?Center(child:Text(Measurements.initials(imageTitle))):Container(),
+          child: imageTitle != null
+              ? Center(child: Text(Measurements.initials(imageTitle)))
+              : Container(),
         ),
-        Padding(padding: EdgeInsets.only(left: 15),),
+        Padding(
+          padding: EdgeInsets.only(left: 15),
+        ),
         Expanded(
           child: Column(
-            mainAxisAlignment:MainAxisAlignment.center,
-            crossAxisAlignment:CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text("$_title",style: TextStyle(fontSize: AppStyle.fontSizeDashboardAvatarDescriptionTitle(),fontWeight: FontWeight.bold),),
-              Text("$_detail",style: TextStyle(fontSize: AppStyle.fontSizeDashboardAvatarDescriptionDescription()),)
+              Text(
+                "$_title",
+                style: TextStyle(
+                    fontSize:
+                        AppStyle.fontSizeDashboardAvatarDescriptionTitle(),
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "$_detail",
+                style: TextStyle(
+                    fontSize: AppStyle
+                        .fontSizeDashboardAvatarDescriptionDescription()),
+              )
             ],
           ),
         )
@@ -35,24 +52,91 @@ class AvatarDescriptionCard extends StatelessWidget {
     );
   }
 }
+
+
+class AvatarDescriptionCardOnButton extends StatelessWidget {
+  final ImageProvider image;
+  final String imageTitle;
+  final String _title;
+  final String _detail;
+
+  AvatarDescriptionCardOnButton(this.image, this._title, this._detail,
+      {this.imageTitle})
+      : assert(image != null || imageTitle != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 15),
+        ),
+        CircleAvatar(
+          backgroundColor: Colors.grey.withOpacity(0.5),
+          radius: AppStyle.dashboardRadiusSmall(),
+          backgroundImage: image,
+          child: imageTitle != null
+              ? Center(child: Text(Measurements.initials(imageTitle)))
+              : Container(),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 15),
+        ),
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "$_title",
+                style: TextStyle(
+                    fontSize:
+                    AppStyle.fontSizeDashboardAvatarDescriptionTitle(),
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "$_detail",
+                style: TextStyle(
+                    fontSize: AppStyle
+                        .fontSizeDashboardAvatarDescriptionDescription()),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class TitleAmountCardItem extends StatelessWidget {
   final Widget title;
   final String _amount;
   final String titleString;
-  TitleAmountCardItem(this._amount,{this.title,this.titleString=""});
+
+  TitleAmountCardItem(this._amount, {this.title, this.titleString = ""});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: (AppStyle.dashboardCardContentHeight()/2)-1,
-      padding: EdgeInsets.symmetric(horizontal:AppStyle.dashboardCardContentPadding()* 1.5),
+      height: (AppStyle.dashboardCardContentHeight() / 2) - 1,
+      padding: EdgeInsets.symmetric(
+          horizontal: AppStyle.dashboardCardContentPadding() * 1.5),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Container(
-            child: titleString.isNotEmpty?Text(titleString,style: TextStyle(fontSize:  AppStyle.fontSizeDashboardTitleAmount())):title,
+            child: titleString.isNotEmpty
+                ? Text(titleString,
+                    style: TextStyle(
+                        fontSize: AppStyle.fontSizeDashboardTitleAmount()))
+                : title,
           ),
           Container(
-            child: Text(_amount,style: TextStyle(fontSize:  AppStyle.fontSizeDashboardTitleAmount()),),
+            child: Text(
+              _amount,
+              style:
+                  TextStyle(fontSize: AppStyle.fontSizeDashboardTitleAmount()),
+            ),
           ),
         ],
       ),
@@ -63,15 +147,18 @@ class TitleAmountCardItem extends StatelessWidget {
 class NoItemsCard extends StatelessWidget {
   final Widget title;
   final VoidCallback action;
-  NoItemsCard(this.title,this.action);
+
+  NoItemsCard(this.title, this.action);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:  EdgeInsets.symmetric(vertical:10),
-      child: Container( height: 10,
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        height: 10,
         decoration: BoxDecoration(
           color: Colors.white.withOpacity(0.1),
-          borderRadius:  BorderRadius.circular(12),
+          borderRadius: BorderRadius.circular(12),
         ),
         child: InkWell(
           borderRadius: BorderRadius.circular(12),
