e7011ddd07e6cbba229280db1055634b2ffac9b9
softdev112 <mappdevm@gmail.com>
2019-08-29 10:21:33 +0200

    Delete file

--- lib/view_models/transaction_state_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class TransactionStateModel extends ChangeNotifier {
-  
-  String _searchField =""; 
-  String get searchField => _searchField;
-  setSearchField(String _search) => _searchField = _search;
-
-}
