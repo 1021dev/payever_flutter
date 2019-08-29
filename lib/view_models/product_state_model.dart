ae31b19d1b85572bda0914a98c468436d4871d00
softdev112 <mappdevm@gmail.com>
2019-08-29 09:36:55 +0200

    delete lib

--- lib/view_models/product_state_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ProductStateModel extends ChangeNotifier {
-  
-  bool _refresh = false;
-  bool get refresh => _refresh;
-  setRefresh(bool refresh) => _refresh = refresh;
-
-}
