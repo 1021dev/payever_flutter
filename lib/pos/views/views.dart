95ba4093ef992182a22adaf310392c79956e6642
softdev112 <mappdevm@gmail.com>
2019-09-02 14:42:48 +0200

    release update v1.0.0 (2)

--- lib/pos/views/views.dart
+++ lib/pos/views/views.dart
@@ -3,6 +3,5 @@ export 'pos_cart.dart';
 export 'pos_order_section.dart';
 export 'pos_products_list_screen.dart';
 export 'product_details.dart';
-export 'pos_screen.dart';
 export 'send_to_device.dart';
 export 'webview_section.dart';
