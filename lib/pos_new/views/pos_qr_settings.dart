1ebb987e00d95e75dd3669e1a3d72dbf78667f02
softdev112 <mappdevm@gmail.com>
2020-07-01 16:28:49 +0200

    FLUT-10: Download QR image

--- lib/pos_new/views/pos_qr_settings.dart
+++ lib/pos_new/views/pos_qr_settings.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
 import 'dart:ui';
+import 'package:date_format/date_format.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +14,7 @@ import 'package:payever/blocs/bloc.dart';
 import 'package:payever/commons/commons.dart';
 import 'package:payever/commons/views/screens/dashboard/new_dashboard/sub_view/blur_effect_view.dart';
 import 'package:payever/pos_new/widgets/pos_top_button.dart';
+import 'package:http/http.dart' as http;
 
 bool _isPortrait;
 bool _isTablet;
@@ -41,6 +46,8 @@ class _PosQRSettingsState extends State<PosQRSettings> {
   String wallpaper;
   String selectedState = '';
   bool isOpened = true;
+
+  var imageData;
 
   @override
   void initState() {
@@ -149,7 +156,6 @@ class _PosQRSettingsState extends State<PosQRSettings> {
             child: CircularProgressIndicator(),
           ): Column(
             children: <Widget>[
-              _toolBar(state),
               Expanded(
                 child: _getBody(state),
               ),
@@ -160,105 +166,147 @@ class _PosQRSettingsState extends State<PosQRSettings> {
     );
   }
 
-  Widget _toolBar(PosScreenState state) {
-    return Container(
-      height: 44,
-      color: Colors.black87,
-      child: Container(),
-    );
-  }
-
   Widget _getBody(PosScreenState state) {
+    dynamic response = state.qrForm;
+    List<Widget> widgets = [];
+    widgets.add(
+      Container(
+        height: 64,
+        color: Color(0xFF424141),
+        child: SizedBox.expand(
+          child: MaterialButton(
+            onPressed: () {
+              setState(() {
+                isOpened = !isOpened;
+              });
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    SvgPicture.asset(
+                      'assets/images/qr.svg',
+                      height: 20,
+                      width: 20,
+                      color: Colors.white,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8),
+                    ),
+                    Text(
+                      'QR Generator',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                ),
+                Icon(
+                  isOpened ? Icons.keyboard_arrow_up: Icons.keyboard_arrow_down,
+                  size: 20,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+
+    if (response != null) {
+      if (response['form'] != null) {
+        dynamic form = response['form'];
+        String contentType = form['contentType'] != null ? form['contentType'] : '';
+        dynamic content = form['content'] != null ? form['content']: null;
+        if (content != null) {
+          dynamic data = content[contentType][0];
+          if (data['data'] != null) {
+            List<dynamic> list = data['data'];
+            for(dynamic w in list) {
+              if (w[0]['type'] == 'image') {
+                if (imageData == null) {
+                  getBlob(w);
+                }
+                Widget imageWidget = isOpened ? Container(
+                  height: 300,
+                  color: Colors.white,
+                  child: imageData != null ? Image.memory(imageData, fit: BoxFit.fitHeight,) :Container()
+                ): Container();
+                widgets.add(imageWidget);
+              } else if (w[0]['type'] == 'text') {
+                Widget textWidget = isOpened ? Container(
+                  height: 64,
+                  padding: EdgeInsets.only(left: 16, right: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        w[0]['value']
+                      ),
+                      MaterialButton(
+                        minWidth: 0,
+                        onPressed: () {
+                        },
+                        height: 20,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        color: Colors.black26,
+                        child: Text(
+                          w[1]['action'],
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ): Container();
+                widgets.add(textWidget);
+
+              }
+            }
+          }
+        }
+      }
+    }
     return Center(
       child: Container(
         padding: EdgeInsets.only(left: 16, right: 16),
-        height: isOpened ? 428.0: 64.0,
+        height: isOpened ? 300.0 + 64.0 * 3.0: 64.0,
         child: BlurEffectView(
           color: Color.fromRGBO(20, 20, 20, 0.2),
           blur: 15,
           radius: 12,
           padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
           child: Column(
-            children: <Widget>[
-              Container(
-                height: 64,
-                color: Color(0xFF424141),
-                child: SizedBox.expand(
-                  child: MaterialButton(
-                    onPressed: () {
-                      setState(() {
-                        isOpened = !isOpened;
-                      });
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            SvgPicture.asset(
-                              'assets/images/qr.svg',
-                              height: 20,
-                              width: 20,
-                              color: Colors.white,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(left: 8),
-                            ),
-                            Text(
-                              'QR Generator',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                              ),
-                            )
-                          ],
-                        ),
-                        Icon(
-                          isOpened ? Icons.keyboard_arrow_up: Icons.keyboard_arrow_down,
-                          size: 20,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              isOpened ? Divider(
-                color: Colors.white30,
-                height: 0,
-                thickness: 0.5,
-              ): Container(),
-              isOpened ? Container(
-                  height: 300,
-                  child: Container()
-              ): Container(),
-              isOpened ? Divider(
-                color: Colors.white30,
-                height: 0,
-                thickness: 0.5,
-              ): Container(),
-              isOpened ? Container(
-                height: 64,
-                color: Color(0xFF424141),
-                child: SizedBox.expand(
-                  child: MaterialButton(
-                    onPressed: () {
-                    },
-                    child: Text(
-                      'Save',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                ),
-              ): Container(),
-            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: widgets.map((e) => e).toList(),
           ),
         ),
       ),
     );
+  }
+
+  Future getBlob(dynamic w) async {
+    var headers = {
+      HttpHeaders.authorizationHeader: 'Bearer ${GlobalUtils.activeToken.accessToken}',
+      HttpHeaders.contentTypeHeader: 'application/json',
+      HttpHeaders.userAgentHeader: GlobalUtils.fingerprint
+    };
+
+    http.get(w[0]['value'],
+        headers:  headers
+    ).then((http.Response response) {
+      print(response);
+      setState(() {
+        imageData = response.bodyBytes;
+      });
+    });
   }
 }
 
