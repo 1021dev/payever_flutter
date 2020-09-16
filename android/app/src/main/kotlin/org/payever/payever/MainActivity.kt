d434b356b0fdcd532c56f3fd9980ec31498abe6a
softdev112 <mappdevm@gmail.com>
2020-09-16 12:50:57 +0200

    FLUT-10: Update Environment

---
+++ android/app/src/main/kotlin/org/payever/payever/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.payever.payever
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Volumes/Source/Source/Development/Flutter/flutter1_20_4"
-export "FLUTTER_APPLICATION_PATH=/Volumes/Work/Payever/flutter"
-export "FLUTTER_TARGET=/Volumes/Work/Payever/flutter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Volumes/Work/Payever/payever"
+export "FLUTTER_TARGET=/Volumes/Work/Payever/payever/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
