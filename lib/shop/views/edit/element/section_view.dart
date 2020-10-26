6cd7188cadde7b5271ecd987721896c11dd92b76
softdev112 <mappdevm@gmail.com>
2020-10-26 09:21:08 +0100

    FLUT-10: Update Section VIew

--- lib/shop/views/edit/element/section_view.dart
+++ lib/shop/views/edit/element/section_view.dart
@@ -76,159 +76,17 @@ class _SectionViewState extends State<SectionView> {
     List<Widget> widgets = [];
     widgets.add(_sectionBackgroundWidget(styleSheet));
     child.children.forEach((child) {
-      Widget widget;
-      switch(child.type) {
-        case 'text':
-          widget = TextView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'button':
-          widget = ButtonView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'image':
-          widget = ImageView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'video':
-          widget = VideoView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'shape':
-          widget = ShapeView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'block':
-          widget = BlockView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'menu':
-          widget = MenuView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'shop-cart':
-          widget = ShopCartView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'shop-category':
-          widget = ShopProductCategoryView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'shop-products':
-          widget = ShopProductsView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'shop-product-details':
-          widget = ShopProductDetailView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'logo':
-          widget = LogoView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        case 'social-icon':
-          widget = SocialIconView(
-            child: child,
-            stylesheets: stylesheets,
-            deviceTypeId: shopPage.stylesheetIds.mobile,
-            sectionStyleSheet: styleSheet,
-          );
-          break;
-        default:
-          print('Special Child Type: ${child.type}');
-          print('Special Styles: ${ stylesheets[shopPage.stylesheetIds.mobile][child.id]}');
-      }
-      if (widget != null) widgets.add(widget);
+      Widget widget = getChild(child);
+      if (widget != null)
+        widgets.add(widget);
     });
-
     // Add Drag Buttons
     if (widgets.isNotEmpty) {
       widgets.add(Positioned(
         bottom: 0,
         left: 0,
         right: 0,
-        child: Container(
-            width: 24,
-            height: 24,
-            alignment: Alignment.bottomCenter,
-            child: GestureDetector(
-                onVerticalDragUpdate: (DragUpdateDetails details) {
-                  setState(() {
-                    double newHeight = details.globalPosition.dy;
-                    RenderBox box = key.currentContext.findRenderObject();
-                    Offset position1 = box
-                        .localToGlobal(Offset.zero); //this is global position
-                    if (newHeight - position1.dy > 30) {
-                      widgetHeight = newHeight - position1.dy;
-                    }
-                    widgetHeight.isNegative
-                        ? Navigator.pop(context)
-                        : controller.add(widgetHeight);
-                  });
-                },
-                behavior: HitTestBehavior.translucent,
-                onVerticalDragDown: (details) {
-                  print('onVerticalDragDown dy = ${details.globalPosition.dy}');
-                },
-                onVerticalDragEnd: (DragEndDetails details) {
-                  editAction();
-                  print('onVerticalDragEnd ');
-                },
-                onVerticalDragStart: (details) {
-                  print('onVerticalDragDown dy = ${details.globalPosition.dy}');
-                },
-                child: Icon(
-                  Icons.arrow_drop_up,
-                  color: Colors.black,
-                ))),
+        child: dragArrow,
       ));
     }
 
@@ -243,6 +101,157 @@ class _SectionViewState extends State<SectionView> {
         ),
       ),
     );
+  }
+
+  Widget getChild(Child child) {
+    Widget widget;
+    switch(child.type) {
+      case 'text':
+        widget = TextView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'button':
+        widget = ButtonView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'image':
+        widget = ImageView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'video':
+        widget = VideoView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'shape':
+        widget = ShapeView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'block':
+        widget = BlockView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'menu':
+        widget = MenuView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'shop-cart':
+        widget = ShopCartView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'shop-category':
+        widget = ShopProductCategoryView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'shop-products':
+        widget = ShopProductsView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'shop-product-details':
+        widget = ShopProductDetailView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'logo':
+        widget = LogoView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      case 'social-icon':
+        widget = SocialIconView(
+          child: child,
+          stylesheets: stylesheets,
+          deviceTypeId: shopPage.stylesheetIds.mobile,
+          sectionStyleSheet: styleSheet,
+        );
+        break;
+      default:
+        print('Special Child Type: ${child.type}');
+        print('Special Styles: ${ stylesheets[shopPage.stylesheetIds.mobile][child.id]}');
+    }
+    return widget;
+  }
+
+  Widget get dragArrow {
+    return Container(
+        width: 24,
+        height: 24,
+        alignment: Alignment.bottomCenter,
+        child: GestureDetector(
+            onVerticalDragUpdate: (DragUpdateDetails details) {
+              setState(() {
+                double newHeight = details.globalPosition.dy;
+                RenderBox box = key.currentContext.findRenderObject();
+                Offset position1 = box
+                    .localToGlobal(Offset.zero); //this is global position
+                if (newHeight - position1.dy > 30) {
+                  widgetHeight = newHeight - position1.dy;
+                }
+                widgetHeight.isNegative
+                    ? Navigator.pop(context)
+                    : controller.add(widgetHeight);
+              });
+            },
+            behavior: HitTestBehavior.translucent,
+            onVerticalDragDown: (details) {
+              print('onVerticalDragDown dy = ${details.globalPosition.dy}');
+            },
+            onVerticalDragEnd: (DragEndDetails details) {
+              editAction();
+              print('onVerticalDragEnd ');
+            },
+            onVerticalDragStart: (details) {
+              print('onVerticalDragDown dy = ${details.globalPosition.dy}');
+            },
+            child: Icon(
+              Icons.arrow_drop_up,
+              color: Colors.black,
+            )));
   }
 
   Widget _sectionBackgroundWidget(SectionStyleSheet styleSheet) {
@@ -296,23 +305,4 @@ class _SectionViewState extends State<SectionView> {
     }
   }
 
-//  {
-//    "id":"7ba51e4f-1453-4523-8ea2-d5ecfdbab002", // UUID
-//    "createdAt":"2020-10-26T06:25:13.890Z",
-//    "targetPageId":"79829a99-b16f-47fb-8ba8-e25ae5c0df40", // Page ID
-//    "affectedPageIds":[
-//      "79829a99-b16f-47fb-8ba8-e25ae5c0df40" // Page ID
-//    ],
-//    "effects":[
-//      {
-//        "type":"stylesheet:update",
-//        "target":"stylesheets:68e0f8ff-453b-4529-ba9a-985deb137067", // shopPage.stylesheetIds.mobile
-//        "payload":{
-//          "f3bae31d-df46-4674-86fc-3fdc68c2d87d":{   // Section (Child ID)
-//          "height":116
-//          }
-//        }
-//      }
-//    ]
-//  }
 }
